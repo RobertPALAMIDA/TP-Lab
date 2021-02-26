48f1d319b75f551e4fc1325453e059e24310dfd4
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-02-26 10:27:33 +0200

    Create B7.c

---
+++ L2/B7.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+
+int GetFlag(int n, int k)
+{
+	((n&(1 << k)) == (1 << k));
+	return (1 & (n >> k));
+}
+
+int main()
+{
+	int N,i,j,k=0;
+	printf("Dati N ");
+	scanf("%d", &N);
+
+	for (i = 1; i <= N; i++)
+		for (j = 7; j >= 0; j--)
+			if (GetFlag(i, j))
+				k = k + 1;
+
+	printf("Suma bitilor este %d", k);
+
+	system("pause");
+	return 0;
+
+}
