51ea42b8c00b75c110c6ca2f193520e34d73322d
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-02-26 10:27:25 +0200

    Create B3.c

---
+++ L2/B3.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int GetFlag(int n)
+{
+	((n&(1 << 31)) == (1 << 31));
+	return (1 & (n >> 31));
+}
+
+int main()
+{
+	int  n,i,k=0;
+	printf("Introduceti numarul ");
+	scanf("%d", &n);
+	for (i = 31; i >= 0; i--)
+		if (n >> i & 1)
+			k = k + 1;
+	if (k == 0)
+		printf("Numarul este nul\n");
+	else if (GetFlag(n))
+		printf("Numarul este negativ\n");
+	else
+		printf("Numarul este pozitiv\n");
+	
+
+	system("pause");
+	return 0;
+
+}
