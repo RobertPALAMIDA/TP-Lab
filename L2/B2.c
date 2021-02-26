ff1020f01e673636b013d82552838001eb0bb93b
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-02-26 10:27:22 +0200

    Create B2.c

---
+++ L2/B2.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int GetFlag(int n, int k)
+{
+	((n&(1 << k)) == (1 << k));
+	return (1 & (n >> k));
+}
+
+int main()
+{
+	int C, N, v[100], i;
+	printf("Introduceti pozitia C ");
+	scanf("%d", &C);
+	printf("Introduceti numarul de numere ");
+	scanf("%d", &N);
+	for (i = 0; i < N; i++)
+	{
+		printf("Dati v[%i]= ", i + 1);
+		scanf("%d", &v[i]);
+	}
+	for (i = 0; i < N; i++)
+		if (GetFlag(v[i], C))
+			printf("%d ", v[i]);
+
+	system("pause");
+	return 0;
+
+}
