bd953464f089bad9fe157f012c5554f021a3761c
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-02-26 10:27:35 +0200

    Create B8.c

---
+++ L2/B8.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+
+int GetFlag(int n, int k)
+{
+	((n&(1 << k)) == (1 << k));
+	return (1 & (n >> k));
+}
+
+int main()
+{
+	int n,k,patrat=0;
+	printf("Introduceti numarul ");
+	scanf("%d", &n);
+	k = n;
+	while (k != 0)
+	{
+		if (k & 1)
+			patrat = patrat + n;
+
+		n = n << 1;
+		k = k >> 1;
+	}
+	printf("Patratul numarului este %d \n", patrat);
+	system("pause");
+	return 0;
+
+}
