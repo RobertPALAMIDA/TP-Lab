6c09d43be1bc3d6e538b01c5424095212a1faca8
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-03-11 19:55:30 +0200

    Create P2.c

---
+++ L3/P2.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct binar {
+	int bit;
+	struct binar *urm;
+}nod;
+
+nod *adaugareInFata(nod *prim, int *bit)
+{
+	nod *p;
+	p = (nod *)malloc(sizeof(nod));
+	p->bit = bit;
+	p->urm = NULL;
+	if (p == NULL)
+		printf("EROARE la alocarea memoriei");
+	else
+		if (prim == NULL)
+			return p;
+		else
+		{
+			p->urm = prim;
+			return p;
+		}
+	return 0;
+}
+
+
+void afisareLista(nod *prim)
+{
+	nod *p;
+	p = prim;
+	while (p != NULL) {
+		printf("%d", p->bit);
+		p = p->urm;
+	}
+}
+
+int GetFlag(int n, int k)
+{
+	return (1 & (n >> k));
+}
+
+int main()
+{
+	nod *prim;
+	prim = NULL;
+	int k, nr, i, bit;
+	printf("Dati numarul ");
+	scanf("%d", &nr);
+	for (i = 0; i <= 7; i++)
+		if (GetFlag(nr, i))
+			k = i;
+	for (i = 0; i <= k; i++)
+	{
+		if (GetFlag(nr, i))
+			bit = 1;
+		else
+			bit = 0;
+
+		prim = adaugareInFata(prim, bit);
+	}
+
+	afisareLista(prim);
+	printf("\n");
+	system("pause");
+	return 0;
+}
