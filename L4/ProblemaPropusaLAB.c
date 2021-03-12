24a52116eadb2eb85c2505b631c5bcccfb20eae3
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2021-03-12 10:33:13 +0200

    Create ProblemaPropusaLAB.c

---
+++ L4/ProblemaPropusaLAB.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+#define VMAX 100
+#define defaultValue -1
+
+int v[VMAX], varf = -1;
+
+void push(int elem)
+{
+	++varf;
+	if (VMAX > varf)
+		v[varf] = elem;
+	else
+		printf("Stiva este plina");
+}
+
+int pop()
+{
+	int a;
+	a = v[varf];
+	v[varf] = defaultValue;
+	varf--;
+	return a;
+}
+
+int GetTop()
+{
+	if (varf >= 0)
+		return v[varf];
+	else
+		return defaultValue;
+}
+
+int main()
+{
+	int x,ok=0,k;
+	printf("Introduceti un numar x= ");
+	scanf("%d", &x);
+	push(x);
+	while (ok == 0)
+	{
+		if (GetTop()>=20)
+		{
+			x = x - 3;
+			push(x);
+			ok = 1;
+		}
+		
+		else if (GetTop() > 12 && GetTop() < 20)
+		{
+			x = x + 1;		
+			push(x);
+		}
+		else
+		{
+			x = pow(x, 2);
+			push(x);
+
+			if (GetTop() >= 20)
+			{
+				x = x - 3;
+				push(x);
+			}
+			else if (GetTop() > 12 && GetTop() < 20)
+			{
+				x = x + 1;
+				push(x);
+			}
+			else
+			{
+				x = pow(x, 2);
+				push(x);
+			}
+		}
+
+	}
+	printf("%d", pop());
+	system("pause");
+	return 0;
+}
